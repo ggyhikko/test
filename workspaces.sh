5a14e2da9fdbd8356cb2e1db5e7e505f1d5af804
root <root@localhost.localdomain>
2017-07-04 17:27:46 +0200

    second commit

---
+++ workspaces.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Alt>1']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Alt>2']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Alt>3']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Alt>4']"
